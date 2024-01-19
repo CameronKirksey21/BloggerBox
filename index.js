d71dcd128c9ebfbb8c064df9e5b68506238af8c6
CameronKirksey21 <Cameron_kirksey21@yahoo.com>
2024-01-19 14:11:43 -0600

    Blog post are loading designated pages

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import _ from 'lodash';
 
 const homeStartingContent =
   'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi ipsa, soluta debitis officiis adipisci aspernatur doloremque eaque et blanditiis voluptatum vel, ad, suscipit at rerum in quaerat facilis explicabo totam!';
@@ -16,14 +17,51 @@ const port = 3000;
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const posts = [];
+
 app.get('/', (req, res) => {
-  res.render('home.ejs', { content: homeStartingContent });
+  res.render('home.ejs', { content: homeStartingContent, posts: posts });
 });
 
-// app.post('/create', (req, res) => {
-//   res.render();
-// });
+app.get('/about', (req, res) => {
+  res.render('about.ejs', { aboutContent: aboutContent });
+});
 
+app.get('/contact', (req, res) => {
+  res.render('contact.ejs', { contactContent: contactContent });
+});
+
+app.get('/create', (req, res) => {
+  res.render('create.ejs');
+});
+
+app.post('/create', (req, res) => {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  };
+
+  posts.push(post);
+
+  res.redirect('/');
+});
+
+app.get('/posts/:postName', (req, res) => {
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+  posts.forEach((post) => {
+    const storedTitle = _.lowerCase(post.title);
+
+    if (storedTitle === requestedTitle) {
+      res.render('post.ejs', {
+        title: post.title,
+        content: post.content,
+      });
+    } else {
+      console.log('Not a Match');
+    }
+  });
+});
 // app.put('/edit', (req, res) => {
 //   res.render();
 // });
