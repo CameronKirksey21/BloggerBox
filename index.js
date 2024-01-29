b654d33f4a0a629c6a0e6d5d329558169989a498
CameronKirksey21 <Cameron_kirksey21@yahoo.com>
2024-01-29 16:18:19 -0600

    complete contact page

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import _ from 'lodash';
 
 const homeStartingContent =
   'Welcome to BloggerBox, your personalized gateway to personal blogging! Are you passionate about sharing your thoughts, experiences, and creativity with yourself? Look no further. BloggerBox is a tailor-made application designed for individuals who aspire to express themselves freely through the art of blogging.  our user-friendly platform provides the perfect space for you to unleash your creativity. With BloggerBox, you have the power to blog about anything that inspires you';
@@ -10,16 +9,42 @@ const aboutContent =
 
 const contactContent = 'Feel free to contact me by the following:';
 
-let lastId = 0;
-
 const app = express();
 const port = 3000;
 
+// Data Center
+let posts = [];
+
+// Post Constructor
+function Post(title, content) {
+  this.title = title;
+  this.content = content;
+  this.rawDate = new Date();
+  this.date = this.rawDate.toLocaleString();
+}
+
+// Add Post
+function addPost(title, content) {
+  let post = new Post(title, content);
+  posts.push(post);
+}
+
+// Delete Post
+function deletePost(index) {
+  posts.splice(index, 1);
+}
+// Edit Post
+function editPost(index, title, content) {
+  posts[index] = new Post(title, content);
+}
+
+// Midleware
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const posts = [];
+// Todo All paths
 
+// Home
 app.get('/', (req, res) => {
   res.render('home.ejs', { content: homeStartingContent, posts: posts });
 });
@@ -32,43 +57,56 @@ app.get('/contact', (req, res) => {
   res.render('contact.ejs', { contactContent: contactContent });
 });
 
+// View Post
+app.get('/view/:id', (req, res) => {
+  let index = req.params.id;
+  let post = posts[index];
+  res.render('view.ejs', {
+    postId: index,
+    title: post.title,
+    content: post.content,
+  });
+});
+
+// Delete Post
+app.post('/delete', (req, res) => {
+  let index = req.body['postId'];
+  deletePost(index);
+  res.redirect('/');
+});
+
+// Edit Post Page
+app.get('/edit/:id', (req, res) => {
+  let index = req.params.id;
+  let post = posts[index];
+  res.render('create.ejs', {
+    postId: index,
+    title: post.title,
+    content: post.content,
+  });
+});
+
+// Update
+app.post('/update', (req, res) => {
+  let title = req.body['title'];
+  let content = req.body['content'];
+  let index = req.body['index'];
+  editPost(index, title, content);
+  res.redirect('/');
+});
+
+// Create Post Page
 app.get('/create', (req, res) => {
   res.render('create.ejs');
 });
 
-app.post('/create', (req, res) => {
-  const newId = (lastId += 1);
-  const post = {
-    id: newId,
-    title: req.body.postTitle,
-    content: req.body.postBody,
-    author: req.body.postAuthor,
-    date: new Date(),
-  };
+// Save Post
+app.post('/save', (req, res) => {
+  let title = req.body['title'];
+  let content = req.body['content'];
 
-  lastId = newId;
-  posts.push(post);
-  res.status(201).redirect('/');
-});
-
-app.get('/posts/:id', (req, res) => {
-  const requestedId = parseInt(req.params.id);
-
-  posts.forEach((post) => {
-    const storedId = post.id;
-
-    if (storedId === requestedId) {
-      res.render('post.ejs', {
-        id: post.id,
-        title: post.title,
-        content: post.content,
-        author: post.author,
-        date: post.date,
-      });
-    } else {
-      console.log('Not a Match');
-    }
-  });
+  addPost(title, content);
+  res.redirect('/');
 });
 
 app.listen(port, () => {
